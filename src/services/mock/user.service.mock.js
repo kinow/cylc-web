b85c9568808e690b9b2ae194e9bbd2b5a26dfbfb
Bruno P. Kinoshita <bruno.kinoshita@niwa.co.nz>
2019-10-30 14:07:30 +1300

    dropme

--- src/services/mock/user.service.mock.js
+++ src/services/mock/user.service.mock.js
@@ -4,10 +4,8 @@ import store from '@/store/index'
 export const UserService = {
 
   getUserProfile () {
-    return new Promise((resolve, reject) => {
-      const user = new User(process.env.USER, ['cylc-developers', 'linux-users'], new Date(), true, `/user/${process.env.USER}/`)
-      return store.dispatch('user/setUser', user)
-    })
+    const user = new User(process.env.USER, ['cylc-developers', 'linux-users'], new Date(), true, `/user/${process.env.USER}/`)
+    return store.dispatch('user/setUser', user)
   }
 
 }
